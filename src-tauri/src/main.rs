c9db03e7290f4d1a4c10fe54a5d82c16b2394140
pwespi <pwespi@pm.me>
2024-01-02 15:26:36 +0100

    add localhost plugin and check for SharedArrayBuffer

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{utils::config::AppUrl, WindowUrl};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,9 +10,25 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    let port = portpicker::pick_unused_port().expect("failed to find unused port");
+
+    let mut context = tauri::generate_context!();
+    let url = format!("http://localhost:{}", port).parse().unwrap();
+    let window_url = WindowUrl::External(url);
+    // rewrite the config so the IPC is enabled on this URL
+    context.config_mut().build.dist_dir = AppUrl::Url(window_url.clone());
+
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
+        .plugin(
+            tauri_plugin_localhost::Builder::new(port)
+                .on_request(|_req, resp| {
+                    resp.add_header("Cross-Origin-Opener-Policy", "same-origin");
+                    resp.add_header("Cross-Origin-Embedder-Policy", "require-corp");
+                })
+                .build(),
+        )
         .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
+        .run(context)
         .expect("error while running tauri application");
 }
